19fe4161c97d80da4ef5c655cddcc35190e479c3
NathanLeysGit <nathanley.personal@gmail.com>
2019-05-14 21:54:29 -0500

    Created global variables for each spider to use

---
+++ spider.py
@@ -0,0 +1,23 @@
+#Imports and Prerequisites
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from crawler import *
+
+
+class Spider:
+
+    #Global variables that each spider can use
+    directory = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+
+    def __init__(self, directory, base_url, domain_name):
+        Spider.directory = directory
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.directory + '/queue.txt'
