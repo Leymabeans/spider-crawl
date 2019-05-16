3f954e4527d298da034e76dfcedfdba0b715e8d3
NathanLeysGit <nathanley.personal@gmail.com>
2019-05-15 21:41:33 -0500

    Created functions to manipulate links

--- spider.py
+++ spider.py
@@ -1,7 +1,7 @@
 #Imports and Prerequisites
 from urllib.request import urlopen
 from link_finder import LinkFinder
-from crawler import *
+from general import *
 
 
 class Spider:
@@ -15,7 +15,7 @@ class Spider:
     queue = set()
     crawled = set()
 
-
+    #Setup and initialization of the spider
     def __init__(self, directory, base_url, domain_name):
         Spider.directory = directory
         Spider.base_url = base_url
@@ -25,4 +25,21 @@ class Spider:
         self.boot()
         self.crawl_page('Spider 1', Spider.base_url)
 
+    #After boot up, perform crawling process
+    def boot(self):
+        create_project_dir(Spider.directory)
+        create_datafiles(Spider.directory, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
 
+    #Pass in url and get all links and add to directories
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + 'crawling' page_url)
+            print('Queue' + str(len(Spider.queue)))
+            print('Crawled' + str(len(Spider.crawled)))
+            print('\n')
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
