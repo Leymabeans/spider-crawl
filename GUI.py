e6230f159725cc752901295f48a9bb656faab9f6
NathanLeysGit <nathanley.personal@gmail.com>
2019-05-30 09:45:31 -0500

    Added tool and status bar

--- GUI.py
+++ GUI.py
@@ -6,12 +6,11 @@ def doNothing():
     print("Do nothing")
 
 
+#Menu and Submenu======================================================
 root = Tk()
-
 menu = Menu(root)
-subMenu = Menu(menu)
-subMenu2 = Menu(menu)
-
+subMenu = Menu(menu, tearoff=0)
+subMenu2 = Menu(menu, tearoff=0)
 root.config(menu=menu)
 
 menu.add_cascade(label="File", menu=subMenu)
@@ -22,4 +21,21 @@ subMenu.add_command(label="Exit", command=doNothing)
 
 menu.add_cascade(label="Edit", command=doNothing)
 subMenu2.add_command(label="Undo Typing", command=doNothing)
+
+
+
+#Toolbar===============================================================
+toolbar = Frame(root, bg="lightblue")
+insert1 = Button(toolbar, text="insert Image", command=doNothing)
+print1 = Button(toolbar, text="Print", command=doNothing)
+
+insert1.pack(side=LEFT, padx=2, pady=2)
+print1.pack(side=LEFT, padx=2, pady=2)
+toolbar.pack(side=TOP, fill=X)
+
+
+
+#Status Bar===========================================================
+status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
+status.pack(side=BOTTOM, fill= X)
 root.mainloop()
