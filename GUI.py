4dc6faf91abf1a993f3df94292b2f2e4933f6508
NathanLeysGit <nathanley.personal@gmail.com>
2019-05-30 08:24:45 -0500

    Created drop down menu

--- GUI.py
+++ GUI.py
@@ -2,8 +2,24 @@
 from tkinter import *
 
 
+def doNothing():
+    print("Do nothing")
+
+
 root = Tk()
 
+menu = Menu(root)
+subMenu = Menu(menu)
+subMenu2 = Menu(menu)
 
+root.config(menu=menu)
 
+menu.add_cascade(label="File", menu=subMenu)
+subMenu.add_command(label="New Project...", command=doNothing)
+subMenu.add_command(label="Delete Project...", command=doNothing)
+subMenu.add_separator()
+subMenu.add_command(label="Exit", command=doNothing)
+
+menu.add_cascade(label="Edit", command=doNothing)
+subMenu2.add_command(label="Undo Typing", command=doNothing)
 root.mainloop()
