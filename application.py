6e9ebe1a26e0eb20da457ade069bf8a5b554dc5b
NathanLeysGit <nathanley.personal@gmail.com>
2019-09-01 17:06:01 -0500

    Restructured and removed objects

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
 #Imports and Functions=====================================
 from tkinter import *
+from messages import *
 import tkinter.messagebox
+
+
+#Displays advanced options to user========================
+def adv_options(): 
+    newwin = Toplevel(root)
+    display = Label(newwin, text="Change the number of threads below")
+    display.pack()  
 
 
 
@@ -8,51 +16,20 @@ import tkinter.messagebox
 
 #Window frames==============================================
 root = Tk()
-root.geometry("600x400")
 information = Frame(root)
 information.pack(side=TOP)
 prompt = Frame(root)
 prompt.pack(side=BOTTOM)
-action = Frame(root)
-action.pack(side=LEFT)
 
 #Prompt and entry==========================================
 title = Label(information, text="Webcrawler Application")
 instructions = Label(information, text="Please insert project name and website URL below")
 project_name = Entry(prompt)
 url = Entry(prompt)
-submit =Button(action, text="Crawl", command=crawl)
-options =Button(action, text="Advanced Options", command=adv_options)
-
-insert1.pack(side=LEFT, padx=2, pady=2)
-print1.pack(side=LEFT, padx=2, pady=2)
-toolbar.pack(side=TOP, fill=X)
+submit =Button(prompt, text="Crawl", command=crawl)
+options =Button(prompt, text="Advanced Options", command=adv_options)
 root.mainloop()
 
-
-
-
-
-#Default messages for the user=============================
-def success():
-    tkinter.messagebox.showinfo('Success', 'Website Successfully Crawled')
-
-def alert():
-    answer = tkinter.messagebox.askquestion('Exit Application', 'Are you sure you want to exit this program?')
-    if answer == 'yes':
-        sys.exit()
-
-
-
-
-#Holding function
-def doNothing():
-    print("Do nothing")
-
-def adv_options(): # new window definition
-    newwin = Toplevel(root)
-    display = Label(newwin, text="Humm, see a new window !")
-    display.pack()  
 
 
 
