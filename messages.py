f391557456e578862a5a53e937b8a357a74225f5
NathanLeysGit <nathanley.personal@gmail.com>
2019-09-03 20:56:42 -0500

    Added icon

--- messages.py
+++
@@ -1,8 +0,0 @@
-#Default messages for the user=============================
-def success():
-    tkinter.messagebox.showinfo('Success', 'Website Successfully Crawled')
-
-def alert():
-    answer = tkinter.messagebox.askquestion('Exit Application', 'Are you sure you want to exit this program?')
-    if answer == 'yes':
-        sys.exit()
