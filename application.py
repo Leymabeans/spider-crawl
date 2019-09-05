021a1f57df3b9d5cb42be67c0835e4336916d031
NathanLeysGit <nathanley.personal@gmail.com>
2019-09-04 20:55:56 -0500

    Built basic application with new layout

--- application.py
+++ application.py
@@ -1,35 +1,64 @@
-#Imports and Functions=====================================
+#Imports===================================
 from tkinter import *
-from messages import *
 import tkinter.messagebox
 
 
-#Displays advanced options to user========================
-def adv_options(): 
+#Displays advanced options to user=========
+def adv_options(event): 
     newwin = Toplevel(root)
     display = Label(newwin, text="Change the number of threads below")
     display.pack()  
 
 
+#Initializes the crawler===================
+def crawl(event):
+    print("Crawling")
 
 
+#Default messages for the user==========
+def success():
+    tkinter.messagebox.showinfo('Success', 'Website Successfully Crawled')
 
-#Window frames==============================================
+def alert():
+    answer = tkinter.messagebox.askquestion('Exit Application', 'Are you sure you want to exit Spider Crawl?')
+    if answer == 'yes':
+        sys.exit()
+
+
+#Presets and variables=====================
+dark = '#0f0f0f'
+light = '#ffffff'
+title_font = ('arial', 32, 'bold')
+instructions_font = ('arial', 9)
+
+#Window configuration======================
 root = Tk()
-information = Frame(root)
-information.pack(side=TOP)
-prompt = Frame(root)
-prompt.pack(side=BOTTOM)
+root.wm_iconbitmap('./crawler.ico')
+root.wm_title('Spider Crawl')
+root.geometry('350x250')
+root.resizable(0, 0)
+root.config(bg=dark)
+info = Frame(root, bg=dark, width=350, height=50)
+prompt = Frame(root, bg=light, width=350, height=200)
 
-#Prompt and entry==========================================
-title = Label(information, text="Webcrawler Application")
-instructions = Label(information, text="Please insert project name and website URL below")
+#Prompt and Entry==========================
+title = Label(info, text="Spider Crawl", font=title_font, bg=dark, fg=light)
+instructions = Label(info, text="Please insert project name and website URL below", font=instructions_font, bg=dark, fg=light)
 project_name = Entry(prompt)
 url = Entry(prompt)
-submit =Button(prompt, text="Crawl", command=crawl)
-options =Button(prompt, text="Advanced Options", command=adv_options)
+submit =Button(prompt, text="Crawl", bg=dark, fg=light)
+options =Button(prompt, text="Advanced Options", bg=dark, fg=light)
+
+
+#Display all elements======================
+info.pack(side=TOP)
+prompt.pack(side=BOTTOM, fill=X)
+title.grid(row=0)
+instructions.grid(row=1)
+project_name.grid(row=2)
+url.grid(row=3)
+submit.grid(row=4)
+submit.bind("<Button-1>", crawl)
+options.grid(row=5)
+options.bind("<Button-1>", adv_options)
 root.mainloop()
-
-
-
-
